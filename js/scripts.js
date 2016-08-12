50a00169dcd9b822f336f0a1510c0a74cb87c082
Jena <jenasancartier@gmail.com>
2016-08-12 09:16:59 -0700

    add more branching code in scripts.js, edit questions in index.html

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,8 @@ $(document).ready(function() {
     var q2Answer = $("input:radio[name=q2]:checked").val();
     var q3Answer = $("input:radio[name=q3]:checked").val();
     var q4Answer = $("input:radio[name=q4]:checked").val();
+    var q5Answer = $("input:radio[name=q5]:checked").val();
+    var q6Answer = $("input:radio[name=q6]:checked").val();
 
     var css = 0;
     var ruby = 0;
@@ -43,21 +45,24 @@ $(document).ready(function() {
     } else if (q3Answer === "nomobile"){
       java -= 3;
     }
-
 //question 4
     if(q4Answer === "playdoh"){
-
+      ruby += 3;
+      css += 5;
     } else if (q4Answer === "legos"){
-
+      php += 3;
+      java += 3
+      csharp +=3;
+      css += 2;
     }
-
 //question 5
-    if(q5Answer === ""){
-
-    } else if (q4Answer ===""){
-
+    if(q5Answer === "design"){
+      css += 4
+      ruby += 1
+    } else if (q5Answer ==="nodesign"){
+      csharp += 3
     }
-    
+
 //question6
     if(q6Answer === "interpret"){
       php += 2;
@@ -67,6 +72,20 @@ $(document).ready(function() {
       csharp += 2;
     }
 
+    var result = "";
+
+    if((css>ruby) && (css>java) && (css>csharp) && (css>php)){
+      result = "<h2>You should take the CSS/DESIGN track!</h2>"
+    } else if((ruby>css) && (ruby>java) && (ruby>csharp) && ruby>php)){
+      result = "<h2>You should take the RUBY/RAILS track!</h2>"
+    } else if((java>css) && (java>ruby) && (java>csharp) && (java>php)){
+      result = "<h2>You should take the JAVA/ANDROID track!</h2>"
+    } else if((csharp>css) && (csharp>ruby) && (csharp>java) && (csharp>php)){
+      result = "<h2>You should take the PHP/DRUPAL track!</h2>"
+    }
+
+
+    $("results").html(result);
   })
 
 
