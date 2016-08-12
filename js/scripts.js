4709ad0d5c24ca18bc1177295f714fbcde2b3ac4
Jena SanCartier <jenasancartier@gmail.com>
2016-08-11 21:11:54 -0700

    begin naming variables in scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+  $("form#quiz").submit(function(event){
+    event.preventDefault();
+    var q1Answer = $("input:radio[name=q1]:checked").val();
+    var q2Answer = $("input:radio[name=q2]:checked").val();
+    var q3Answer = $("input:radio[name=q3]:checked").val();
+    var q4Answer = $("input:radio[name=q4]:checked").val();
+
+
+    
+  })
+
+
+
+});
